09de44db81f282a6a190aa4e5609797145fdd7e9
Itsupport <Itsupport@SUP4981-PLKR100>
2022-12-01 13:07:38 +0100

    Initialize folder Party

---
+++ Learn-CSharp/Przyjecie/Przyjecie/DinnerParty.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przyjecie
+{
+    public class DinnerParty
+    {
+        public int NumberOfPeople;
+        public decimal CostOfBeveragesPerPerson;
+        public decimal CostOfDecorations;
+        public const int CostOfFoodPerPerson = 25;
+
+
+        public void CalculateCostOfDecorations()
+        { 
+        }
+
+        public static void CalculateCost() { }
+
+        public bool SetHealthyOption() {
+
+            return false;
+        }
+
+    }
+}
